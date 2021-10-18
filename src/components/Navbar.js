9a2f1cd0d63b151be2973031c44a94f735ac7f0e
Web Developer <web.newdiscoverytech@gmail.com>
2021-10-18 17:15:42 -0600

    Primer commit - App terminada

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import {Menu, Container, Button, MenuItem} from 'semantic-ui-react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+export const Navbar = () => {
+
+    const router = useRouter()
+
+    return (
+        <Menu
+          inverted
+          borderless
+          attached
+          style={{ padding: ".3rem", marginBottom: "20px" }}
+        >
+            <Container>
+                <Menu.Item>
+                    <Link href='/'>
+                        <img src="/favicon.ico" alt="" />
+                    </Link>
+                </Menu.Item>
+                <Menu.Menu position="right">
+                    <Menu.Item>
+                        <Button
+                          primary
+                          size="mini"
+                          onClick={ () => router.push('/tasks/new')}
+                        >
+                            New Task
+                        </Button>
+                    </Menu.Item>
+                </Menu.Menu>
+            </Container>
+        </Menu>
+    )
+}
